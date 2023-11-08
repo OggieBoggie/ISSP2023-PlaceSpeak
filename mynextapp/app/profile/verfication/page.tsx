d9231915c74dcab0121db000b0df8983f4e9777d
tommy <tommy.nguyen.vanc@gmail.com>
2023-11-08 15:26:38 -0800

    Account page

---
+++ mynextapp/app/profile/verfication/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Sidebar from '../../components/Sidebar';
+
+const UsersPage: React.FC = () => {
+    return (
+        <div>
+            <Sidebar />
+        </div>
+    )
+};
+
+export default UsersPage;
