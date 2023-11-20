b01e683ee577148029c8ed4213e3d7aa4380ead2
melodynunmawi <esthermld007@gmail.com>
2023-11-19 22:32:39 -0800

    Resolve conflicts in page.tsx

--- mynextapp/app/profile/verfication/page.tsx
+++ mynextapp/app/profile/verfication/page.tsx
@@ -1,18 +1,101 @@
-import Sidebar from '../../components/Sidebar';
-
-import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import Profile from "../components/profile";
+import Badges from "../components/Badges";
+import UserInfo from "../components/UserInfo";
+import NewsFeed from "../components/NewsFeed";
+import dynamic from "next/dynamic";
+import { getServerSession } from "next-auth";
 
-const UsersPage: React.FC = async () => {  
-    const session = await getServerSession();
-    if (session && session.user) {
-        return (
-            <div>
-                <Sidebar />
-            </div>
-        )
+const DynamicMapWithLocation = dynamic(
+  () => import("../components/MapWithLocation"),
+  {
+    ssr: false, // This will load the component only on client side
+    loading: () => (
+      <p className="text-lg font-bold text-center text-blue-500">Loading...</p>
+    ),
+  }
+);
+
+export default async function Component() {
+  interface PointProps {
+    latitude: number;
+    longitude: number;
+  }
+
+  async function get_nbhd({ latitude, longitude }: PointProps) {
+    "use server";
+    // Call API to get the neighborhood boundary
+    try {
+      const response = await fetch(
+        `http://127.0.0.1:8000/myapp/api/van_nbhd/?latitude=${latitude}&longitude=${longitude}`
+      );
+      const data = await response.json();
+
+      if (data && data.geom && data.geom.coordinates) {
+        const reversedCoords = data.geom.coordinates[0][0].map((coord: any) => [
+          coord[1],
+          coord[0],
+        ]);
+        return reversedCoords;
+      }
+    } catch (error) {
+      console.error("Error fetching neighborhood data:", error);
     }
-    return redirect("/");
-};
+  }
 
-export default UsersPage;
+  async function update_user_location({ latitude, longitude }: PointProps) {
+    "use server";
+    const session = await getServerSession();
+    fetch(
+      `http://127.0.0.1:8000/myapp/api/user_location/${session?.user?.email}/`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          latitude: latitude,
+          longitude: longitude,
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Location saved:", data);
+      })
+      .catch((error) => {
+        console.error("Error saving location:", error);
+      });
+  }
+
+  const session = await getServerSession();
+  if (session && session.user) {
+    return (
+      <div
+        className="bg-gray-100 flex flex-col min-h-screen"
+        style={{ minHeight: "calc(100vh - 3.25rem)" }}
+      >
+        <div className="flex flex-col md:flex-row md:flex-grow">
+          <div className="flex-1 p-4 md:max-w-md">
+            <Profile />
+            <Badges />
+          </div>
+
+          <div className="w-full md:w-2/3 p-4 mt-4 md:mt-8">
+            <UserInfo />
+            <div className="flex-grow relative">
+              <DynamicMapWithLocation
+                getNbhdAction={get_nbhd}
+                updateUsrLocAction={update_user_location}
+              />
+            </div>
+            <div className="mt-4">
+              <NewsFeed />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return redirect("/");
+}
