2ba1068622d8920d396acb8266c7f8cca6e197ea
Louie <causingljbcit@gmail.com>
2023-11-26 14:33:02 -0800

    Updated the right api to use for backend, changed the badges to display only 2 rows and 3 columns

--- mynextapp/app/components/Badges.tsx
+++ mynextapp/app/components/Badges.tsx
@@ -3,17 +3,19 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useSession } from "next-auth/react";
 
-
-
 function Badges({}) {
     const [badges, setBadges] = useState([]);
     const { data: session } = useSession();
     const userEmail = session?.user?.email;
+
     useEffect(() => {
         const fetchUserBadges = async () => {
             try {
-                const response = await axios.get(`http://localhost:8000/api/get-badges/${userEmail}`); // Update with your Django API URL
-                setBadges(response.data); // Assuming the data is an array of badge objects
+                const response = await axios.get(`http://127.0.0.1:8000/myapp/api/get-badges/${userEmail}`);
+                // Limit the number of badges to 6
+                const limitedBadges = response.data.slice(0, 6);
+                setBadges(limitedBadges);
+                console.log("Badges:", limitedBadges);
             } catch (error) {
                 console.error("Error fetching user badges:", error);
             }
@@ -26,11 +28,12 @@ function Badges({}) {
 
     return (
         <div className="flex flex-col items-center">
-            <div className="bg-white p-8 rounded-lg shadow-md w-96 flex flex-col items-center p-8">
+            <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm flex flex-col items-center">
                 <h2 className="text-xl font-semibold mb-4">Badges</h2>
-                <div className="flex space-x-4 mb-4 justify-between">
+                {/* Create a grid container with 2 rows and 3 columns */}
+                <div className="grid grid-cols-3 gap-4 mb-4">
                     {badges.map((badge, index) => (
-                        <img key={index} src={`/id.svg`} alt={`Verified`} className="w-16 h-16 rounded-full object-cover" />
+                        <img key={index} src={badge.image} alt={badge.name} className="w-16 h-16 rounded-full object-cover" />
                     ))}
                 </div>
                 <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200 focus:ring-opacity-50">See All</button>
@@ -40,5 +43,3 @@ function Badges({}) {
 }
 
 export default Badges;
-// Now mark this as a client component
-export const BadgesClient = 'use client';
